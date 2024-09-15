2e11d2807c712025c52ee427d552ca65257b8c4c
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-15 13:28:55 +0500

    add onChange State listener

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+
+            Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("blue amount \(blurAmount)")
+                }
+
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
+        
         .padding()
     }
 }
