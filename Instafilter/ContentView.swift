8224254bc9da974d6e5e2b1ff35f0a8e4fc513be
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-09-16 19:51:42 +0500

    apply crystallize effect

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -26,9 +26,9 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         // Apply filters
         let context = CIContext()
-        let currentFilter = CIFilter.pixellate()
+        let currentFilter = CIFilter.crystallize()
         currentFilter.inputImage = beginImage
-        currentFilter.scale = 5
+        currentFilter.radius = 50
         // output CI Image
         guard let outputImage = currentFilter.outputImage else { return }
         
